bb3ac776ff938a922b88e65f3c5742d289d0ac8f
theRabbi <tipson664@gmail.com>
2023-06-13 11:41:19 +0100

    filter wards

--- app/script_handler.php
+++ app/script_handler.php
@@ -5,44 +5,42 @@ require('./ward.php');
 $lgas_all = $GLOBALS['app_list_strings']['lga_list'];
 $state = $GLOBALS['app_list_strings']['state_list'];
 
-function getLGA($lgas_all){
+function getLGA($lgas_all)
+{
 
-    if(isset($_GET['state_id'])){
-        $state=substr($_GET['state_id'], 0, 2);
-        $state=strtolower($state);
+    if (isset($_GET['state_id'])) {
+        $state = substr($_GET['state_id'], 0, 2);
+        $state = strtolower($state);
 
-        $lgas=array_filter($lgas_all, function($lga) use ($state){
+        $lgas = array_filter($lgas_all, function ($lga) use ($state) {
             return substr($lga, 0, 2) == $state;
         }, ARRAY_FILTER_USE_KEY);
-        
+
         echo json_encode($lgas);
     }
-
 }
-if(isset($_GET['state_id'])) getLGA($lgas_all);
-if(isset($_GET['lga_id'])) getWards();
+if (isset($_GET['state_id'])) getLGA($lgas_all);
+if (isset($_GET['lga_id'])) getWards();
 
 // $polling_units_all = $GLOBALS['app_list_strings']['polling_unit_list'];
 // $lgas_all = $GLOBALS['app_list_strings']['lga_list'];
 
 
-function getWards(){
-    echo "Ward";
+function getWards()
+{
+
+    // $lga = $_GET['lga_id'];
+    // echo $lga;
     $wards_all = $GLOBALS['app_list_strings']['wards_list'];
-    if(isset($_GET['lga_id'])){
-        $lga=$_GET['lga_id'];
-        $wards=array_filter($wards_all, function($ward) use ($lga){
-           return substr($ward, 0, (count($lga)-1)) == $lga;
+    //  echo json_encode($wards_all);
+    if (isset($_GET['lga_id'])) {
+        $lga = $_GET['lga_id'];
+        $wards = array_filter($wards_all, function ($ward) use ($lga) {
+            return substr($ward, 0, (strlen($lga))) == $lga;
         }, ARRAY_FILTER_USE_KEY);
 
         echo json_encode($wards);
-    }else{
+    } else {
         echo "Hello Baddy";
     }
 }
-
-
-
-
-
-
--- main.js
+++ main.js
@@ -1,79 +1,71 @@
+$(document).ready(function () {
+  // alert("welcome ")
+  $(".select-state-select2").select2();
 
+  $(".select-lga-select2").select2();
 
+  $(".select-ward-select2").select2();
 
-$(document).ready(function() {
+  $(".select-supervisor-select2").select2();
 
-    // alert("welcome ")
-    $(".select-state-select2").select2();
+  // INCIDENCE REPORT PANEL FIELDS
+  $(".select-election-type-select2").select2();
 
-    $(".select-lga-select2").select2();
+  $(".incidence-sub-category").select2();
+  $(".select-category-select2").select2();
 
-    $(".select-ward-select2").select2();
+  // $('body').on('click', '.select2-results__option', function(){
+  //     alert('Yes');
+  // })
 
-    $(".select-supervisor-select2").select2();
+  $("#state_c").on("change", function () {
+    fetch(
+      "http://localhost/popup/app/script_handler.php?state_id=" + this.value
+    )
+      .then((e) => {
+        return e.json();
+      })
+      .then((res) => {
+        // console.log(res)
+        $.each(res, function (index, item) {
+          // console.log(index);
+          $("#lga_c").append(
+            $("<option>", {
+              value: item,
+              text: item,
+            })
+          );
+        });
+        // $('#lga_c').append('<option>'+res+'</option>')
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
 
+  $("#lga_c").on("change", function () {
+    let lga_label = getSelectedText(this);
+    let state = document.getElementById("state_c");
+    alert(state);
+    let state_label = getSelectedText(state);
+    alert(state_label);
+    lga_label = state_label + "_" + lga_label;
+    fetch("http://localhost/popup/app/script_handler.php?lga_id=" + lga_label)
+      .then((e) => {
+        return e.json();
+      })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
 
-    // INCIDENCE REPORT PANEL FIELDS
-    $(".select-election-type-select2").select2();
+  function getSelectedText(elt) {
+    if (elt.selectedIndex == -1) return null;
 
-    $(".incidence-sub-category").select2();
-    $(".select-category-select2").select2();
-
-
-    // $('body').on('click', '.select2-results__option', function(){
-    //     alert('Yes');
-    // })
-
-    $("#state_c").on('change', function(){
-
-        fetch('http://localhost/inec_crm/custom_scripts/app/script_handler.php?state_id='+this.value)
-        .then(e=>{
-            return e.json()
-        })
-        .then(res=>{
-            // console.log(res)
-            $.each(res, function(index, item){
-                // console.log(index);
-                $('#lga_c').append($('<option>', {
-                    value: item,
-                    text: item
-                }));
-            });
-            // $('#lga_c').append('<option>'+res+'</option>')
-        })
-        .catch(err=>{
-            console.log(err)
-        })
-    })
-
-    $("#lga_c").on('change', function(){
-        let lga_label=getSelectedText(this);
-        let state=document.getElementById('state_c')
-        alert(state)
-        let state_label=getSelectedText(state);
-        alert(state_label)
-        lga_label=state_label+'_'+lga_label;
-        fetch('http://localhost/inec_crm/custom_scripts/app/script_handler.php?lga_id='+lga_label)
-        .then(e => {
-            return e.json()
-        })
-        .then(res => {
-            console.log(res);
-        }).catch(err => {
-            console.log(err);
-        })
-    })
-
-    function getSelectedText(elt) {
-        if (elt.selectedIndex == -1)
-            return null;
-
-        let label = elt.options[elt.selectedIndex].text;
-        return label;
-    }
-    
-
+    let label = elt.options[elt.selectedIndex].text;
+    return label;
+  }
 });
-
-
-
